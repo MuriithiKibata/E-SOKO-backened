e98cd4989cb820f4cedd448dfc73f2d5ff330813
aasane kariuki <franciskariki053@gmail.com>
2024-07-10 16:06:30 +0300

    changes

--- app.py
+++ app.py
@@ -1,56 +1,38 @@
-<<<<<<< HEAD
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
-from pathlib import Path
-from fastapi.middleware.cors import CORSMiddleware
+import os
+from datetime import timedelta
 
-
-app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"])
-
-
-@app.get('/')
-def read_root():
-    return {"Hello" : "world"}
-
-
-@app.get('/orders')
-def orders():
-    
-    orders = Orders.find_all()
-    
-    return orders
-
-
-
-@app.get("/get_image")
-async def get_image():
-    image_path = Path("image.jpg")
-    if not image_path.is_file():
-        return {"error": "Image not found on the server"}
-    return FileResponse(image_path)
-
-
-@app.post('/users')
-def users(data: UsersModel):
-    
-    users = Users(data.name, data.email, data.password, data.address) 
-    
-
-    
-
-
-=======
 from flask import Flask
 from flask_migrate import Migrate
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
 from models import db, User
 
 
 app = Flask(__name__)
+api = Api(app)
+
 
 migrate = Migrate(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
+
+CORS(app)
+
+migrate = Migrate(app, db, render_as_batch=True)
+
+bcrypt = Bcrypt(app)
+
+jwt = JWTManager(app)
+
+
+
 db.init_app(app)
->>>>>>> d0d49fb5e2e78ce8b0db4d34414d8be9e961a14d
+
