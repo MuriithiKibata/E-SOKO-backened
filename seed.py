010a39bcc48a738dd820491e3c5fae553b0f10b4
YvonneNdungu <ndunguyvonne10@gmail.com>
2024-07-12 15:35:54 +0300

    add serialize only

--- seed.py
+++ seed.py
@@ -1,4 +1,12 @@
-from models import db, User, Product, Order, OrderItem, Cart
-from app import appdb, app
+from models import db, User, Product, Order, Cart
+from app import app
+
+with app.app_context():
+
+    print("Creating restaurants...")
+    shack = Restaurant(name="Karen's Pizza Shack", address='address1')
+    bistro = Restaurant(name="Sanjay's Pizza", address='address2')
+    palace = Restaurant(name="Kiki's Pizza", address='address3')
+    restaurants = [shack, bistro, palace]
 
 
